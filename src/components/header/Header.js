c6a422dfb5fdc6587af07e098846a040d4260831
hyejun <hyejun0608@naver.com>
2020-11-29 02:12:12 +0900

    [UPDATE] 검색시 타입설정 & router 추가

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,22 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
 import * as S from './style';
 import { Logo, Search } from '../../assets';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
+    const [searchType, setSearchType] = useState('전체');
+    const [isTypeClick, setIsTypeClick] = useState(false);
+
+    const onTypeClick = e => {
+        const clickedType = e.target.dataset.type;
+        if(clickedType === 'all') {
+            setSearchType('전체');
+        }
+        else if(clickedType === 'book') {
+            setSearchType('책');
+        }
+        else {
+            setSearchType('서점');
+        }
+    }
+
+    const onTypeBtnClick = e => {
+        setIsTypeClick(!isTypeClick);
+    }
+
     return (
         <S.Size>
-            <img src={ Logo } alt="탐책"/>
-            <S.Range>
-                <div>▼</div>
-                <div>전체</div>
-            </S.Range>
-            <S.Search placeholder="검색어를 입력해주세요" />
-            <S.SearchImg>
-                <img src={ Search } alt="검색" />
-            </S.SearchImg>
-            <S.List>책 등록</S.List>
-            <S.List>마이페이지</S.List>
-            <S.List>로그인</S.List>
+            <div>
+                <Link to='/'>
+                    <img src={ Logo } alt="탐책"/>
+                </Link>
+                <S.Range onClick={onTypeBtnClick}>
+                    <S.More boolean={isTypeClick}>▼</S.More>
+                    <div>{ searchType }</div>
+                </S.Range>
+                <S.Search placeholder="검색어를 입력해주세요" />
+                <S.SearchImg>
+                    <img src={ Search } alt="검색" />
+                </S.SearchImg>
+                <S.List>
+                    <Link to='/upload/book'>책 등록</Link>
+                </S.List>
+                <S.List>
+                    <Link to='/mypage/user'>마이페이지</Link>
+                </S.List>
+                <S.List>
+                    <Link to='/login'>로그인</Link>
+                </S.List>
+            </div>
+            <S.TypeUl boolean={isTypeClick}>
+                    <S.TypeLi data-type='all' onClick={onTypeClick}>전체</S.TypeLi>
+                    <S.TypeLi data-type='book' onClick={onTypeClick}>책</S.TypeLi>
+                    <S.TypeLi data-type='bookStore' onClick={onTypeClick}>서점</S.TypeLi>
+            </S.TypeUl>
         </S.Size>
     )
 }
--- src/components/header/style.js
+++ src/components/header/style.js
@@ -5,16 +5,23 @@ export const Size = style.div `
     min-width: 1440px;
     height: 52px;
     background-color: #7B91FF;
-    display: flex;
-    align-items: center;
-    & > img:first-child {
+    > div {
+        height: 100%;
+        display: flex;
+        align-items: center;
+    }
+    > div > a > img:first-child {
         width: 50px;
         margin-left: 180px;
+        margin-top: 10px;
     }
 `;
 export const List = style.div `
     font-size: 12px;
     margin-right: 120px;
+    > a {
+        color: black;
+    }
 `;
 export const Search = style.input `
     width: 302px;
@@ -30,12 +37,12 @@ export const Search = style.input `
         color: black;
         font-size: 10px;
     }
-    & > div:first-child {
+    > div:first-child {
         font-size: 10px;
     }
 `;
 export const Range = style.div `
-    width: 56px;
+    width: 60px;
     height: 24px;
     display: flex;
     background-color: white;
@@ -43,15 +50,32 @@ export const Range = style.div `
     border-right: none;
     align-items: center;
     margin-left: 118px;
-    & > div:first-child {
-        font-size: 10px;
-        margin-left: 9px;
-    }
-    & > div:last-child {
+    > div:last-child {
         font-size: 7px;
         margin-left: 10px;
         margin-right: 7px;
     }
+`;
+export const More = style.p `
+    font-size: 8px;
+    margin-left: 9pt;
+    transform: rotate(${({boolean}) => boolean ? 180 : 0}deg);
+    transition: 0.5s;
+`;
+export const TypeUl = style.ul `
+    box-sizing: border-box;
+    width: 67px;
+    height: 60px;
+    border: 1px solid black;
+    border-radius: 3px;
+    background-color: white;
+    padding: 5px 0 0 0;
+    margin: -15px 0 0 348px;
+    display: ${({boolean}) => boolean ? 'block' : 'none'};
+`;
+export const TypeLi = style.li `
+    font-size: 8px;
+    text-align: center;
 `;
 export const SearchImg = style.div `
     width: 19px;
@@ -63,7 +87,7 @@ export const SearchImg = style.div `
     justify-content: center;
     align-items: center;
     margin-right: 180px;
-    & > img {
+    > img {
         width: 7.5px;
         height: 8px;
     }
