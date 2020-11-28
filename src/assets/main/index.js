c44a0ce5bda99e697d4af30b91aa24e3b1af4219
hyejun <hyejun0608@naver.com>
2020-11-29 02:07:34 +0900

    [ADD] BookSlider Img 추가

---
+++ src/assets/main/index.js
@@ -0,0 +1,5 @@
+export { default as Img1 } from './Img1.png';
+export { default as Img2 } from './Img2.png';
+export { default as Img3 } from './Img3.png';
+export { default as Img4 } from './Img4.png';
+export { default as Img5 } from './Img5.png';
