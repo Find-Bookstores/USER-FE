ee37c889cee0a4e7be492f41b019be521010d699
hyejun <hyejun0608@naver.com>
2020-11-29 02:10:07 +0900

    [ADD] 추가된 Img 경로 추가

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,1 +1,6 @@
 export { Logo, Search } from './header';
+export { BSadmin, BSuser } from './signup';
+export { Link } from './book';
+export { BookImg } from './bookInven';
+export { Img1, Img2, Img3, Img4, Img5 } from './main';
+export { WhiteStar, YellowStar } from './storeInfo';
