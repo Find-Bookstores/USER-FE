cc2a7e1396db4c67352056f3518c841dc1dfdc34
hyejun <hyejun0608@naver.com>
2020-12-02 23:20:37 +0900

    [UPDATE] 유저 마이페이지 정보 불러오는 api & 비밀번호 수정 api 연동

--- src/components/mypage/user/UserMyPage.js
+++ src/components/mypage/user/UserMyPage.js
@@ -1,8 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 import * as S from './style';
 import BookStore from './BookStore';
+import axios from 'axios';
 
 const UserMyPage = () => {
+    const [data, setData] = useState('');
+    const [password, setPassword] = useState('');
+    const [CheckPw, setCheckPw] = useState('');
+    axios.get('http://10.156.147.138:8888/user/mypage', {
+        headers: {
+            'Authorization': localStorage.getItem('token')
+        }
+    })
+    .then((res) => {
+        setData(res);
+        console.log('ok');
+    })
+    .catch((err) => {
+        console.log(err);
+    })
+
+    const onModifyBtnClick = e => {
+        if(password === CheckPw) {
+            axios.put('http://10.156.147.138:8888/user/mypage/update', {
+                body: {
+                    password
+                },
+                headers: {
+                    token: localStorage.getItem('token')
+                }
+            })
+            .then((res) => {
+                alert('비밀번호가 수정되었습니다');
+                console.log('ok');
+            })
+            .catch((err) => {
+                console.log(err);
+            })
+        }
+    }
+
+    const checkPw = e => {
+        setPassword(e.target.value);
+    }
+    const checkCpw = e => {
+        setCheckPw(e.target.value);
+    }
+
     return (
         <S.Size>
             <S.Title>
@@ -15,67 +59,36 @@ const UserMyPage = () => {
                 <S.InfoUl>
                     <S.InfoLi>
                         <div>닉네임</div>
-                        <S.InfoInput disabled value='name'/>
+                        <S.InfoInput disabled value={data.name}/>
                     </S.InfoLi>
                     <S.InfoLi>
                         <div>아이디</div>
-                        <S.InfoInput disabled value='id'/>
+                        <S.InfoInput disabled value={data.id}/>
                     </S.InfoLi>
                     <S.InfoLi>
                         <div>비밀번호</div>
-                        <S.PwInput type='password' />
+                        <S.PwInput type='password' onChange={checkPw}/>
                     </S.InfoLi>
                     <S.InfoLi>
                         <div>비밀번호 재확인</div>
-                        <S.PwInput type='password' />
+                        <S.PwInput type='password' onChange={checkCpw} />
                     </S.InfoLi>
-                    <S.ModifyBtn>정보수정</S.ModifyBtn>
+                    <S.ModifyBtn onClick={onModifyBtnClick}>정보수정</S.ModifyBtn>
                 </S.InfoUl>
             </S.InfoSize>
             <S.StarSize>
                 <S.Star>즐겨찾기 서점</S.Star>
                 <S.StarLine />
                 <S.Stores>    
-                    <BookStore 
-                        storename='탐책 서점'
-                        time='11:00 ~ 9:00'
-                        number='042-000-0000'
-                    />
-                    <BookStore 
-                        storename='탐책 서점'
-                        time='11:00 ~ 9:00'
-                        number='042-000-0000'
-                    />
-                    <BookStore 
-                        storename='탐책 서점'
-                        time='11:00 ~ 9:00'
-                        number='042-000-0000'
-                    />
-                    <BookStore 
-                        storename='탐책 서점'
-                        time='11:00 ~ 9:00'
-                        number='042-000-0000'
-                    />
-                    <BookStore 
-                        storename='탐책 서점'
-                        time='11:00 ~ 9:00'
-                        number='042-000-0000'
-                    />
-                    <BookStore 
-                        storename='탐책 서점'
-                        time='11:00 ~ 9:00'
-                        number='042-000-0000'
-                    />
-                    <BookStore 
-                        storename='탐책 서점'
-                        time='11:00 ~ 9:00'
-                        number='042-000-0000'
-                    />
-                    <BookStore 
-                        storename='탐책 서점'
-                        time='11:00 ~ 9:00'
-                        number='042-000-0000'
-                    />
+                    {data.map(data => {
+                        return(
+                            <BookStore 
+                                storename={data.storename}
+                                time={data.time}
+                                number={data.number}
+                            />
+                        )
+                    })}
                 </S.Stores>
             </S.StarSize>
         </S.Size>
