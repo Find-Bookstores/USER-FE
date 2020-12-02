c7b7a39791bc00a1ed57bda9f743cab763750ccb
hyejun <hyejun0608@naver.com>
2020-12-02 23:19:22 +0900

    [UPDATE] 책 등록 api 연동

--- src/components/book/UploadBook.js
+++ src/components/book/UploadBook.js
@@ -1,8 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import * as S from './style';
 import { Link } from '../../assets';
+import axios from 'axios';
 
 const UploadBook = () => {
+    const [name, setName] = useState('');
+    const [author, setAuthor] = useState('');
+    const [publisher, setPublisher] = useState('');
+    const [image, setImage] = useState('');
+
+    const onUploadBtnClick = e => {
+        if(name !== '' && author !== '' && publisher !== '' && image !== '') {
+            axios.post('http://10.156.147.138:8888/book/inputbook', {
+                Param: {
+                    name,
+                    author,
+                    publisher,
+                    image
+                },
+                header: {
+                    token: localStorage.getItem('token')
+                }
+            })
+        }
+        else {
+            alert('모든 칸을 채워주세요');
+        }
+    }
+
+    const checkName = e => {
+        setName(e.target.value);
+    }
+    const checkAuthor = e => {
+        setAuthor(e.target.value);
+    }
+    const checkPublisher = e => {
+        setPublisher(e.target.value);
+    }
+
     return (
         <S.Size>
             <S.Title>
@@ -10,9 +45,9 @@ const UploadBook = () => {
                 <S.UnderLine />
             </S.Title>
             <S.Input>
-                <S.Form placeholder='등록하실 책의 이름을 입력해주세요' />
-                <S.Form placeholder='등록하실 책의 저자를 입력해주세요' />
-                <S.Form placeholder='등록하실 책의 출판사를 입력해주세요' />
+                <S.Form placeholder='등록하실 책의 이름을 입력해주세요' onChange={checkName}/>
+                <S.Form placeholder='등록하실 책의 저자를 입력해주세요' onChange={checkAuthor} />
+                <S.Form placeholder='등록하실 책의 출판사를 입력해주세요' onChange={checkPublisher} />
                 <S.Img>
                     <div>
                         <S.ImgFlie>등록하실 책의 사진 파일을 첨부해주세요</S.ImgFlie>
@@ -23,7 +58,7 @@ const UploadBook = () => {
                     </div>
                 </S.Img>
             </S.Input>
-            <S.Upload>책 등록</S.Upload>
+            <S.Upload onClick={onUploadBtnClick}>책 등록</S.Upload>
         </S.Size>
     )
 }
